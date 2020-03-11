cad31ee74a73777be746d7826536d3b82cde655f
Alan.Lin <eric820209@gmail.com>
2020-03-12 02:00:27 +0800

    加入專案檔案。

---
+++ SendValueAddress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendValueAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
